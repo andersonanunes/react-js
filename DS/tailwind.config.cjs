4239709b4b1e533906ca3e4a5a71046e414a4f91
andersonanunes <andersonanunes@hotmail.com>
2022-10-12 20:06:05 -0300

    commit inicial design system

---
+++ DS/tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#C4C4CC',
+      'gray-100': '#E1E1E6',
+
+      'cyan-500': '#81D8F7',
+      'cyan-300': '#98DDF5',
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}

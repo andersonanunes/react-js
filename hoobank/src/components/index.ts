ee106cfa7d5e787d0ca86cd9146fe55257021b08
andersonanunes <andersonanunes@hotmail.com>
2022-10-26 19:29:48 -0300

    upload inicial projeto Hoobank

---
+++ hoobank/src/components/index.ts
@@ -0,0 +1,24 @@
+import Navbar from './NavBar'
+import Hero from './Hero'
+import Biling from './Biling'
+import CardDeal from './CardDeal';
+import Business from './Business';
+import Clients from './Clients';
+import Cta from './Cta';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import Footer from './Footer';
+
+
+export {
+    Navbar,
+    Hero,
+    Biling,
+    CardDeal,
+    Business,
+    Clients,
+    Cta,
+    Stats,
+    Testimonials,
+    Footer,
+}

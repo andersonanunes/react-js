cd6c624cb7f1f436a01422ab9bf94cfc3a796b5f
andersonanunes <andersonanunes@hotmail.com>
2022-11-01 02:09:13 -0300

    finalizando os componentes

--- hoobank/src/components/Button.tsx
+++ hoobank/src/components/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ styles }) => {
+const Button = ({ styles }: any) => {
   return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary rounded-[10px] outline-none ${styles}`}>
       Get Started
     </button>
   )

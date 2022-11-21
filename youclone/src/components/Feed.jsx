496801085bc361eec041558781162d105c118a87
andersonanunes <andersonanunes@hotmail.com>
2022-11-21 10:25:27 -0300

    feat(YOUCLONE): update inicial do projeto

---
+++ youclone/src/components/Feed.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar, Videos } from './components';
+
+
+const Feed = () => (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+        <Sidebar/>
+        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2022 WhoDesigners
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+          New <span style={{ color: '#F31503' }}>
+            Videos
+          </span>
+        </Typography>
+      </Box>
+
+      <Videos videos={[]} />
+
+    </Stack>
+  )
+
+export default Feed

82a248a2059e2eaa6f1737a7a688b1bbad186ae4
andersonanunes <andersonanunes@hotmail.com>
2023-01-09 09:22:23 -0300

    feat: inclusao do dark theme e do eslint

---
+++ admin-material/src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useCallback, useContext, useMemo, useState } from "react";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme, DarkTheme } from "../themes";
+import { Box } from "@mui/system";
+
+interface IThemeContextData {
+    themeName: 'light' | 'dark';
+    toogleTheme: () => void;
+}
+
+interface IAppThemeProviderProps {
+    children: React.ReactNode;
+}
+
+const ThemeContext = createContext({} as IThemeContextData );
+
+export const useAppThemeContext = () => {
+    return useContext(ThemeContext);
+}
+
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({ children }) => {
+    const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
+
+    const toogleTheme = useCallback(() => {
+        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+    }, [])
+    const theme = useMemo(() => {
+        if (themeName === 'light') return LightTheme;
+        return DarkTheme;
+    }, [themeName])
+
+    return(
+        <ThemeContext.Provider value={{ themeName, toogleTheme }}>
+            <ThemeProvider theme={theme}>
+                <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+                    {children}
+                </Box>
+            </ThemeProvider>
+        </ThemeContext.Provider>
+    )
+}

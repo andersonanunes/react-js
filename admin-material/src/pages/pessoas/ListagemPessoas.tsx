76852643cc057bb906e7694171006a24e58f25a2
andersonanunes <andersonanunes@hotmail.com>
2023-01-15 20:34:30 -0300

    feat: criando a tabela e exibindo resultados

--- admin-material/src/pages/pessoas/ListagemPessoas.tsx
+++ admin-material/src/pages/pessoas/ListagemPessoas.tsx
@@ -1,27 +1,47 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { TableContainer, Table, Paper } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
+
+import { IListagemPessoa, PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 import { SearchBar } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { BaseLayout } from '../../shared/layouts';
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
 
 export const ListagemPessoas: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const {debounce} = useDebounce();
+
+    const [rows, setRows] = useState<IListagemPessoa[]>([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
     useEffect(() => {
-        PessoasService.getAll(1, busca)
-            .then ((result) => {
-                if (result instanceof Error) {
-                    alert(result.message);
-                } else {
-                    console.log(result);
-                }
-            });
+    
+        setIsLoading(true);
+    
+        debounce(() => {
+            PessoasService.getAll(1, busca)
+                .then ((result) => {
+                    setIsLoading(false);
+                    if (result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        console.log(result);
+                        setRows(result.data);
+                        setTotalCount(result.totalCount);
+                    }
+                });
+        });
     }, [busca]);
 
     return (
@@ -37,6 +57,29 @@ export const ListagemPessoas: React.FC = () => {
                 />
             }    
         >
+            <TableContainer component={Paper} variant='outlined' sx={{ margin: 1, width: 'auto' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ações</TableCell>
+                            <TableCell>Nome Completo</TableCell>
+                            <TableCell>Email</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+
+                        {rows.map( row => (
+                            <TableRow key={row.id}>
+                                <TableCell>{row.id}</TableCell>
+                                <TableCell>{row.nomeCompleto}</TableCell>
+                                <TableCell>{row.email}</TableCell>
+                            </TableRow>
+                        ))}
+
+
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </BaseLayout>
     );
 };
